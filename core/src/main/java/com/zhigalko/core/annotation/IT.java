e25449957dd5e325f710e709df1ce680c162989a
Eugene Zhigalko <y.zhyhalka@andersenlab.com>
2024-09-16 22:28:57 +0400

    refactored application yml files

---
+++ core/src/main/java/com/zhigalko/core/annotation/IT.java
@@ -0,0 +1,17 @@
+package com.zhigalko.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+public @interface IT {
+}
