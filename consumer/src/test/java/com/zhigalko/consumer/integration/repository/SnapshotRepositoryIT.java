5594c674c456f799947b8ba27795f29b48b486bb
Evgeniy Zhigalko <y.zhyhalka@andersenlab.com>
2024-10-11 19:06:47 +0400

    fix: disabled logs storage in files for tests

--- consumer/src/test/java/com/zhigalko/consumer/integration/repository/SnapshotRepositoryIT.java
+++ consumer/src/test/java/com/zhigalko/consumer/integration/repository/SnapshotRepositoryIT.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -17,6 +18,7 @@ import static com.zhigalko.consumer.util.TestDataUtil.getSnapshot;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Testcontainers
 class SnapshotRepositoryIT {
 
